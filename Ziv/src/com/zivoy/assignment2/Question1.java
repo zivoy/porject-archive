1e0f7060cf8efbe16b49605a89afa151dad4bb82
zivoy <ziv132@gmail.com>
2020-03-06 01:27:32 -0500

    Question1 remade with gui

--- Ziv/src/com/zivoy/assignment2/Question1.java
+++ Ziv/src/com/zivoy/assignment2/Question1.java
@@ -2,18 +2,15 @@ package com.zivoy.assignment2;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
 import java.util.Random;
-import java.util.Scanner;
 
 /**
  * @author Ziv Shalit
  * @Assignment 2
  * @Question 1
  * @Date 2020/02/28
- *
+ * <p>
  * rewrote with gui on 2020/03/05
  */
 
@@ -28,117 +25,146 @@ public class Question1 {
         // make a layout
         frame.setLayout(new GridLayout(2, 1));
 
+        // label for text
         JLabel text = new JLabel();
-
+        // add it to panel
         panel1.add(text);
 
+        // create buttons for each option
         JButton rock = new JButton("Rock");
         JButton paper = new JButton("Paper");
         JButton scissors = new JButton("Scissors");
 
-
+        // add them to the second panel
         panel2.add(rock);
         panel2.add(paper);
         panel2.add(scissors);
 
+        // add the 2 panels to the frame
         frame.add(panel1);
         frame.add(panel2);
 
+        // set the text on the label to set a default window size
         text.setText("Rock, Paper, Scissors, lets find the winner between the 2, on 3");
+        // center the text
         text.setHorizontalAlignment(JTextField.CENTER);
+
+        // set the default close operation
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-
-
-
-        // make scanner and random objects for making random numbers and taking input
+        // make random object for making random numbers
         Random random = new Random();
 
+        // make the game object
         Game game = new Game(frame, random, text, panel2);
 
+        // create listeners for each button that will evaluate the game
         rock.addActionListener(e -> game.round(0));
         paper.addActionListener(e -> game.round(1));
         scissors.addActionListener(e -> game.round(2));
 
-
+        // pack window
         frame.pack();
+        // show it
         frame.setVisible(true);
     }
 
-
-    static class Game extends Thread{
+    // main game class / contains logic
+    static class Game {
+        // keep the text label
         JLabel text;
+        // keep the panel containing the buttons
         JPanel buttonPanel;
+        // keep the frame
         JFrame frame;
-        int hand=-1;
+        // keep the users last hand
+        int hand = -1;
+
+        // keep a random object
         Random random;
+
+        // the default string to explain the game
         final String intro = "Rock, Paper, Scissors, lets find the winner between the 2";
 
-        public Game(JFrame frame,Random random,JLabel textfield, JPanel buttons){
+        // constructor that puts stuff in place
+        public Game(JFrame frame, Random random, JLabel textfield, JPanel buttons) {
+            // puts values in variables
             this.text = textfield;
             this.buttonPanel = buttons;
-            this.buttonPanel.setVisible(true);
             this.frame = frame;
             this.random = random;
-            this.text.setText(toLabel(this.intro+"\npick an option"));
+
+            // make buttons visible
+            this.buttonPanel.setVisible(true);
+            // set the text on button
+            this.text.setText(toLabel(this.intro + "\npick an option"));
         }
 
-        public void round(int item){
+        // the function that handles game logic given the players hand
+        public void round(int item) {
+            // hide the buttons
             this.buttonPanel.setVisible(false);
+
+            // keep the players hand
             this.hand = item;
-            this.countDown();
+            // computer chooses an option
             int computer = random.nextInt(3);
-            this.text.setText(toLabel("You throw " + nameItem(this.hand)+"\n" +
+
+            // set the label to explain what happened
+            this.text.setText(toLabel("You throw " + nameItem(this.hand) + "\n" +
                     "The computer threw " + nameItem(computer)));
 
-            if (computer != this.hand)
-                if (determineWinner(computer))
-                    System.out.println("You won!");
-                else System.out.println("You lost.");
-             else System.out.println("TIE!");
-             this.buttonPanel.setVisible(true);
-            this.text.setText(toLabel(this.intro+"\npick an option"));
+            if (computer != this.hand) // check if tie
+                if (determineWinner(computer)) // if not check if player won
+                    // tell player they won
+                    this.dialog("You won!", "You have won\nWould you like to play again?");
+                    // otherwise tell player they lost
+                else this.dialog("You lost.", "You have lost\nWould you like to play again?");
+                // otherwise say it was a tie
+            else this.dialog("TIE", "The game TIED\nWould you like to play again?");
+
+            // show the buttons
+            this.buttonPanel.setVisible(true);
+            // change text back to explenation string
+            this.text.setText(toLabel(this.intro + "\npick an option"));
         }
 
-        private void countDown(){
-            // countdown and wait 1 second between each number
-            this.text.setText(toLabel(this.intro+"\n1"));
-            pause(1000);
-            this.text.setText(toLabel(this.intro+"\n2"));
-            pause(1000);
-            this.text.setText(toLabel(this.intro+"\n3"));
-        }
-
-        // function that sleeps exits if interrupted
-        private void pause(long time){
-            this.frame.repaint();
-            try {
-                Thread.sleep(time);
-            } catch (InterruptedException ignored){ this.close();}
-            this.frame.repaint();
-        }
-
-        private void close(){
+        // function for closing the window
+        private void close() {
             this.frame.dispatchEvent(new WindowEvent(this.frame, WindowEvent.WINDOW_CLOSING));
         }
 
+        // dialog function
+        private void dialog(String title, String message) {
+            // take the title and message and display it in a yes no dialog
+            int dialogResult = JOptionPane.showConfirmDialog(null,
+                    message, title, JOptionPane.YES_NO_OPTION);
+
+            // if the user clicked no
+            if (dialogResult == JOptionPane.NO_OPTION) {
+                // close the app
+                this.close();
+            }
+            // otherwise do nothing
+        }
+
         // function that converts an int to a string
-        private String nameItem(int item){
-            switch (item){
-                case 0:
+        private String nameItem(int item) {
+            switch (item) {
+                case 0: // rock
                     return "Rock";
-                case 1:
+                case 1: // paper
                     return "Paper";
-                case 2:
+                case 2: // scissors
                     return "Scissors";
-                default:
+                default: // otherwise
                     return "NON VALID INTEGER";
             }
         }
 
         // functions for which number beats which
-        private boolean determineWinner(int opponent){
-            switch (this.hand){
+        private boolean determineWinner(int opponent) {
+            switch (this.hand) {
                 case 0:  // if rock
                     if (opponent == 2)  // if opponent scissors
                         return true;  // you won
@@ -156,8 +182,10 @@ public class Question1 {
         }
     }
 
-    public static String toLabel(String string){
-        string = "<html>"+string+"</html>";
-        return string.replaceAll("\n","<br/>");
+    public static String toLabel(String string) {
+        // add html tag to start and end
+        string = "<html>" + string + "</html>";
+        // replace and return newline character with break line html tag
+        return string.replaceAll("\n", "<br/>");
     }
 }
