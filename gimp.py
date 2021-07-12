833663254da64829718f15522af4df79d3ade3d3
zivoy <ziv132@gmail.com>
2021-07-12 15:22:31 -0400

    colour seporator done

---
+++ gimp.py
@@ -0,0 +1,44 @@
+# import matplotlib.pyplot as plt
+
+from PIL import Image
+import numpy as np
+import os
+
+if not os.path.exists("parts"):
+    os.mkdir("parts")
+
+
+def equals(value, target, lamda=0):
+    return abs(value - target) <= lamda
+
+
+def unique(array):
+    s = array.shape
+    tmp = array.reshape((s[0] * s[1], s[2]))
+    return np.unique(tmp, axis=0)
+
+
+file = "sample.png"
+im = Image.open(file)
+im = im.convert('RGBA')
+# mask = ImageEnhance.Sharpness(im).enhance(10)
+
+data = np.array(im)
+red, green, blue, alpha = data.T
+
+arr = np.array([0, 0, 0, 255], dtype=np.uint8)
+emptyMask = np.tile(arr, (data.shape[0], data.shape[1], 1))
+
+for j, i in enumerate(unique(data)):
+    maskData = emptyMask.copy()
+    # if i == (255, 255, 255, 255):
+    #     continue
+    lamda = 0
+    print(i)
+    highlighted = equals(red, i[0], lamda) & equals(blue, i[2], lamda) & equals(green, i[1], lamda)
+    maskData[..., :-1][highlighted.T] = (255, 255, 255)
+
+    mask = Image.fromarray(maskData)
+    mask.save(os.path.join("parts", f"{'.'.join(file.split('.')[:-1])}-mask-{j}.png"))
+    # plt.imshow(im2)
+    # plt.show()
