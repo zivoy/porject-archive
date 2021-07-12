bfecc32e8a7dafe8aa04defa279a124938a2caeb
zivoy <ziv132@gmail.com>
2021-07-12 17:41:12 -0400

    separate from b+w no AA image

--- main.py
+++ main.py
@@ -10,28 +10,76 @@ def equals(value, target, lamda=0):
     return abs(value - target) <= lamda
 
 
-def unique(array):
-    s = array.shape
-    tmp = array.reshape((s[0] * s[1], s[2]))
-    return np.unique(tmp, axis=0)
+# load image
 
-
-file = "sample.png"
+# file = "sample.png"
+# file = "small.png"
 im = Image.open(file)
 im = im.convert('RGBA')
 
+# island detection
 data = np.array(im)
-red, green, blue, alpha = data.T
+islands = list()
+x = 0
+y = 0
+while True:
+    found = False
+    while x < data.shape[1]:
+        while y < data.shape[0]:
+            # print(i)
+            if (data[y][x][:3] == (0, 0, 0)).all():  # is black
+                found = True
+                # print(i, j, data[i][j])
+                break
+            y += 1
+        if found:
+            break
+        x += 1
+        y = 0
+    else:
+        # no blacks found exit
+        break
 
+    # find connected blacks
+    island = np.tile(False, data.shape[:2])
+    checked = set()
+    tocheck = [(y, x)]
+    while tocheck:
+        i = tocheck.pop()
+        if (data[i[0]][i[1]][:3] == (0, 0, 0)).all():
+            island[i[0]][i[1]] = True
+            for j in range(-1, 2):
+                for k in range(-1, 2):
+                    # not this
+                    if j == k == 0:
+                        continue
+
+                    # not out of border
+                    t1 = i[0] + j
+                    t2 = i[1] + k
+                    if (t1 < 0 or t2 < 0) and (t1 >= data.shape[0] or t2 >= data.shape[1]):
+                        continue
+
+                    # not checked
+                    t3 = (t1, t2)
+                    if t3 in checked:
+                        continue
+                    # to check
+                    tocheck.append(t3)
+
+        checked.add(i)
+
+    # remove island
+    data[..., :-1][island] = (255, 255, 255)
+    islands.append(island)
+
+# masking
 arr = np.array([0, 0, 0, 255], dtype=np.uint8)
-emptyMask = np.tile(arr, (data.shape[0], data.shape[1], 1))
+emptyMask = np.tile(arr, (*data.shape[:2], 1))
 
-for j, i in enumerate(unique(data)):
+for j, i in enumerate(islands):
     maskData = emptyMask.copy()
-    lamda = 0
-    print(i)
-    highlighted = equals(red, i[0], lamda) & equals(blue, i[2], lamda) & equals(green, i[1], lamda)
-    maskData[..., :-1][highlighted.T] = (255, 255, 255)
+    maskData[..., :-1][i] = (255, 255, 255)
 
     mask = Image.fromarray(maskData)
     mask.save(os.path.join("parts", f"{'.'.join(file.split('.')[:-1])}-mask-{j}.png"))
